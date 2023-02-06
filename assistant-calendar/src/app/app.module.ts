877c5b513717d33d2173d9279b01a9bc423dae18
Ghazal Sadeghpour <gsade077@uottawa.ca>
2023-02-06 15:49:47 -0500

    fixed module.ts

--- assistant-calendar/src/app/app.module.ts
+++ assistant-calendar/src/app/app.module.ts
@@ -7,21 +7,29 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from "@angular/material/icon";
+import { CreateSchedulePageComponent } from "./create-schedule-page/create-schedule-page.component";
+import { CreateScheduleComponent } from './create-schedule-page/create-schedule/create-schedule.component';
+import { ImportScheduleComponent } from './create-schedule-page/import-schedule/import-schedule.component';
+import { SubmitScheduleComponent } from './create-schedule-page/submit-schedule/submit-schedule.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent
-  ],
-  imports: [
-    MatIconModule,
-    MatButtonModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomepageComponent,
+        CreateSchedulePageComponent,
+        CreateScheduleComponent,
+        ImportScheduleComponent,
+        SubmitScheduleComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        MatIconModule,
+        MatButtonModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule
+    ]
 })
 export class AppModule { }
