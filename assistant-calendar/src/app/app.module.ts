11ef8588b6deeb0ea5b67af9a3ebdf4ec2f6a09a
Mahdi Chiboub <71799789+mchib031@users.noreply.github.com>
2023-02-07 13:49:51 -0500

    implement drag and drop using ngx-file-drop package

--- assistant-calendar/src/app/app.module.ts
+++ assistant-calendar/src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from "@angular/material/icon";
 import { CreateSchedulePageComponent } from "./create-schedule-page/create-schedule-page.component";
 import { CreateScheduleComponent } from './create-schedule-page/create-schedule/create-schedule.component';
 import { ImportScheduleComponent } from './create-schedule-page/import-schedule/import-schedule.component';
 import { SubmitScheduleComponent } from './create-schedule-page/submit-schedule/submit-schedule.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatRippleModule} from '@angular/material/core';
 import { AddScheduleComponent } from './create-schedule-page/create-schedule/add-schedule/add-schedule.component';
 import { ImportSyllabusComponent } from './create-schedule-page/import-schedule/import-syllabus/import-syllabus.component';
+
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {NgxFileDropModule} from "ngx-file-drop";
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRippleModule } from '@angular/material/core';
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -35,6 +40,9 @@ import { ImportSyllabusComponent } from './create-schedule-page/import-schedule/
     providers: [],
     bootstrap: [AppComponent],
     imports: [
+        FormsModule,
+        HttpClientModule,
+        NgxFileDropModule,
         MatIconModule,
         MatButtonModule,
         BrowserModule,
