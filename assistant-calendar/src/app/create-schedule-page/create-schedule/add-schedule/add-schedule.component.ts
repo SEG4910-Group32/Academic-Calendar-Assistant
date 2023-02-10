510e31aaa253af16e36a119213362cea99fd30b3
Ghazal Sadeghpour <gsade077@uottawa.ca>
2023-02-09 20:36:35 -0500

    getting task type from the dialog

--- assistant-calendar/src/app/create-schedule-page/create-schedule/add-schedule/add-schedule.component.ts
+++ assistant-calendar/src/app/create-schedule-page/create-schedule/add-schedule/add-schedule.component.ts
@@ -2,6 +2,8 @@ import { Component, Inject, Optional } from '@angular/core';
 import { Deliverable } from '../deliverable';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CreateScheduleComponent } from '../create-schedule.component';
+import { mockSchedules } from '../mock-schedules';
 
 @Component({
   selector: 'app-add-schedule',
@@ -12,17 +14,28 @@ export class AddScheduleComponent {
 
   action?:string;
   local_data:any;
-
   constructor(
-    public dialogRef: MatDialogRef<AddScheduleComponent>,
-    //@Optional() is used to prevent error if no data is passed
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: Deliverable) {
-    this.local_data = {...data};
-    console.log(this.local_data);
-    this.action = this.local_data.action;
+    public dialogRef: MatDialogRef<CreateScheduleComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {type:string, dueDate:string},
+  ) {
+    mockSchedules.push( 
+      {type:data.type, dueDate:data.dueDate},
+      )
   }
 
-  addNewDeliverable(){
-    this.dialogRef.close({event:this.action,data:this.local_data});
+  onNoClick(): void {
+    this.dialogRef.close();
   }
+  // constructor(
+  //   public dialogRef: MatDialogRef<AddScheduleComponent>,
+  //   //@Optional() is used to prevent error if no data is passed
+  //   @Optional() @Inject(MAT_DIALOG_DATA) public data: Deliverable) {
+  //   this.local_data = {...data};
+  //   console.log(this.local_data);
+  //   this.action = this.local_data.action;
+  // }
+
+  // addNewDeliverable(){
+  //   this.dialogRef.close({event:this.action,data:this.local_data});
+  // }
 }
--- assistant-calendar/src/app/create-schedule-page/create-schedule/create-schedule.component.ts
+++ assistant-calendar/src/app/create-schedule-page/create-schedule/create-schedule.component.ts
@@ -1,9 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild , Inject} from '@angular/core';
 import { FormControl } from '@angular/forms';
-import {MatDialog} from '@angular/material/dialog';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { AddScheduleComponent } from './add-schedule/add-schedule.component';
 import { Deliverable } from './deliverable';
 import { mockSchedules } from './mock-schedules';
+
+
 
 @Component({
   selector: 'app-create-schedule',
@@ -11,14 +13,33 @@ import { mockSchedules } from './mock-schedules';
   styleUrls: ['./create-schedule.component.css']
 })
 export class CreateScheduleComponent {
+  type: string | undefined;
+  dueDate: string | undefined;
+
+  tmpType: string| undefined;
+  tmpDueDate: string| undefined;
   deliverables = mockSchedules;
   constructor(public dialog: MatDialog) {}
 
-  openDialog() {
-    this.dialog.open(AddScheduleComponent, {
-      height: '300px',
-      width: '500px',
+  // openDialog() {
+  //   this.dialog.open(AddScheduleComponent, {
+  //     height: '300px',
+  //     width: '500px',
+  //   });
+  // }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddScheduleComponent, {
+      data: {task: this.type, dueDate: this.dueDate},
     });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      //mockSchedules.push({this.type, this.dueDate})
+      this.type = result;
+      this.dueDate = result.dueDate;
+      mockSchedules.push({type:result, dueDate:"2023-02-09"})
+    });
+    
   }
 }
 
