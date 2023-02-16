435fcec50a9a0be685af5045e32beff0cf9e3939
Mahdi Chiboub <71799789+mchib031@users.noreply.github.com>
2023-02-15 21:47:18 -0500

    remove previous navigation btns code

--- assistant-calendar/src/app/create-schedule-page/create-schedule-page.component.ts
+++ assistant-calendar/src/app/create-schedule-page/create-schedule-page.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';
 
 @Component({
   selector: 'app-create-schedule-page',
   templateUrl: './create-schedule-page.component.html',
-  styleUrls: ['./create-schedule-page.component.css'],
-  animations: [
-    // animation triggers go here
-  ]
+  styleUrls: ['./create-schedule-page.component.css']
 })
 export class CreateSchedulePageComponent {
 
-  public currentContainer = 0;
-
-  public changeContainer(delta: number): void {
-    this.currentContainer += delta;
-  }
 }
