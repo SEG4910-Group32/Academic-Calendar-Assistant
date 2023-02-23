77777f8d399168800c365fa57d31478cfbdf235d
Sam Sunny <sampsunny1995@gmail.com>
2023-02-23 16:01:24 -0500

    findSchedule FrontEnd

--- assistant-calendar/src/app/app-routing.module.ts
+++ assistant-calendar/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FindScheduleComponent } from './find-schedule/find-schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'find-schedule', component: FindScheduleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- assistant-calendar/src/app/app.module.ts
+++ assistant-calendar/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FindScheduleComponent } from './find-schedule/find-schedule.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindScheduleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
