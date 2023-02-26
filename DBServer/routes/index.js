154bac6296980476af1c0a0f1bb499c5dd5a6ceb
Sam Sunny <sampsunny1995@gmail.com>
2023-02-26 12:48:16 -0500

    added router calls for Schedules and Users

--- DBServer/routes/index.js
+++ DBServer/routes/index.js
@@ -10,39 +10,33 @@ const db = mongoose.connection
 // ---------------------------------
 /* GET Events. */
 // ---------------------------------
-router.get('/', async function(req, res, next) {
+router.get('/event/', async function (req, res, next) {
 
   let results = await db.collection("Events").find({}).toArray();
-  
+
   res.send(results).status(200);
 });
 
 // ---------------------------------
 /* Create Event. */
 // ---------------------------------
-router.post('/create', async function(req, res, next) {
+router.post('/event/create', async function (req, res, next) {
 
   let results = await db.collection("Events").insertOne(req.body);
-  
+
   res.send(results).status(204);
 });
 
 // ---------------------------------
 /* Update Event. */
 // ---------------------------------
-router.patch("/:id", async (req, res) => {
+router.patch("/event/:id", async (req, res) => {
   const query = { _id: mongoose.Types.ObjectId(req.params.id) };
-  // const updates = {
-  //   $push: { comments: req.body }
-  // };
-  // newEvent = JSON.parse(req.body);
   delete req.body._id;
-  eventToUpdate = {summary: req.body.summary, description:req.body.description, location: req.body.location, start_date: req.body.start_date, end_date: req.body.end_date};
   let collection = await db.collection("Events");
-  // let result = await collection.updateOne(query, updates);
   console.log(query)
   console.log(req.body)
-  let result = await collection.updateOne(query, {$set:  req.body}, {upsert:true})
+  let result = await collection.updateOne(query, { $set: req.body }, { upsert: true })
 
   res.send(result).status(200);
 });
@@ -50,10 +44,118 @@ router.patch("/:id", async (req, res) => {
 // ---------------------------------
 /* Delete Event. */
 // ---------------------------------
-router.delete("/:id", async (req, res) => {
+router.delete("/event/:id", async (req, res) => {
   const query = { _id: mongoose.Types.ObjectId(req.params.id) };
   console.log(query);
   const collection = db.collection("Events");
+  let result = await collection.deleteOne(query);
+
+  res.send(result).status(200);
+});
+
+
+
+
+
+
+
+
+
+// ---------------------------------
+/* GET Schedule. */
+// ---------------------------------
+router.get('/schedule/', async function (req, res, next) {
+
+  let results = await db.collection("Schedules").find({}).toArray();
+
+  res.send(results).status(200);
+});
+
+// ---------------------------------
+/* Create Schedule. */
+// ---------------------------------
+router.post('/schedule/create', async function (req, res, next) {
+
+  let results = await db.collection("Schedules").insertOne(req.body);
+
+  res.send(results).status(204);
+});
+
+// ---------------------------------
+/* Update Schedule. */
+// ---------------------------------
+router.patch("/schedule/:id", async (req, res) => {
+  const query = { _id: mongoose.Types.ObjectId(req.params.id) };
+  delete req.body._id;
+  let collection = await db.collection("Schedules");
+  console.log(query)
+  console.log(req.body)
+  let result = await collection.updateOne(query, { $set: req.body }, { upsert: true })
+
+  res.send(result).status(200);
+});
+
+// ---------------------------------
+/* Delete Schedule. */
+// ---------------------------------
+router.delete("/schedule/:id", async (req, res) => {
+  const query = { _id: mongoose.Types.ObjectId(req.params.id) };
+  console.log(query);
+  const collection = db.collection("Schedules");
+  let result = await collection.deleteOne(query);
+
+  res.send(result).status(200);
+});
+
+
+
+
+
+
+
+
+
+// ---------------------------------
+/* GET Users. */
+// ---------------------------------
+router.get('/user/', async function (req, res, next) {
+
+  let results = await db.collection("Users").find({}).toArray();
+
+  res.send(results).status(200);
+});
+
+// ---------------------------------
+/* Create Schedule. */
+// ---------------------------------
+router.post('/user/create', async function (req, res, next) {
+
+  let results = await db.collection("Users").insertOne(req.body);
+
+  res.send(results).status(204);
+});
+
+// ---------------------------------
+/* Update Schedule. */
+// ---------------------------------
+router.patch("/user/:id", async (req, res) => {
+  const query = { _id: mongoose.Types.ObjectId(req.params.id) };
+  delete req.body._id;
+  let collection = await db.collection("Users");
+  console.log(query)
+  console.log(req.body)
+  let result = await collection.updateOne(query, { $set: req.body }, { upsert: true })
+
+  res.send(result).status(200);
+});
+
+// ---------------------------------
+/* Delete Schedule. */
+// ---------------------------------
+router.delete("/user/:id", async (req, res) => {
+  const query = { _id: mongoose.Types.ObjectId(req.params.id) };
+  console.log(query);
+  const collection = db.collection("Users");
   let result = await collection.deleteOne(query);
 
   res.send(result).status(200);
