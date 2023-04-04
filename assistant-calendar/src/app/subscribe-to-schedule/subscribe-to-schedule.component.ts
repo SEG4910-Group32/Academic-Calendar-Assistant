68af1239c5ffcbda7dd5f1aaaa5c1c24a9221024
JeremyFang022 <jfang022@uottawa.ca>
2023-04-04 17:18:49 -0400

    redesigned subscribe-to-schedule display

---
+++ assistant-calendar/src/app/subscribe-to-schedule/subscribe-to-schedule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe-to-schedule',
+  templateUrl: './subscribe-to-schedule.component.html',
+  styleUrls: ['./subscribe-to-schedule.component.css']
+})
+export class SubscribeToScheduleComponent {
+
+}
