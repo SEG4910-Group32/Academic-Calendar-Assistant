3750339a7e6b88425113fd3070254fc97100f8f5
Ghazal Sadeghpour <gsade077@uottawa.ca>
2023-10-30 01:12:12 -0400

    changed the ui structure of showing schedules in user profile page

--- assistant-calendar/src/app/profile-page/profile-page.component.ts
+++ assistant-calendar/src/app/profile-page/profile-page.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component ,ChangeDetectionStrategy} from '@angular/core';
 import {NgFor} from '@angular/common';
 import {MatListModule} from '@angular/material/list';
 import { HttpClient } from '@angular/common/http';
 import { Deliverable } from '../create-schedule-page/create-schedule/deliverable';
 import { Observable } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatTable } from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -11,15 +15,21 @@ import { Observable } from 'rxjs';
   
 })
 export class ProfilePageComponent {
+  displayedColumns: string[] = ['Name', 'Id', 'Edit', 'Delete'];
   Schedules: Observable<Deliverable[]>;
   // private endpoint = '"https://academic-calendar-backend.onrender.com/api/schedules/';
   //private endpoint = 'https://academic-calendar-backend.onrender.com/api/owner/65175b6ec4cfd5b8effe44ee';
-  private endpoint = 'https://academic-calendar-backend.onrender.com/api/schedules/owner/651e3c7ed23a153e3fdce497'
+  private endpoint = 'https://academic-calendar-backend.onrender.com/api/schedules/'
+   dataSource: any;
+   items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
   constructor(private http: HttpClient) { 
     this.Schedules= this.http.get<Deliverable[]>(this.endpoint);// ['SEG3102', 'SEG3101'];
   
   }
-
   
+
+  // ngOnInit(): void{
+  //   this.dataSource = this.Schedules;
+  // }
  //Schedules: string[] = this.http.get<Deliverable[]>(this.endpoint);// ['SEG3102', 'SEG3101'];
 }
