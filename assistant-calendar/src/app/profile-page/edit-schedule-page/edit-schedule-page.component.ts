9b2142c4b45dcbf7619add432f8176e87c013a6f
Ghazal Sadeghpour <gsade077@uottawa.ca>
2023-10-30 23:55:49 -0400

    started working on edit schedule page
    added the scrolling feature to the table, connected to the backend

--- assistant-calendar/src/app/profile-page/edit-schedule-page/edit-schedule-page.component.ts
+++ assistant-calendar/src/app/profile-page/edit-schedule-page/edit-schedule-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
-
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Deliverable } from 'src/app/create-schedule-page/create-schedule/deliverable';
+import { Event } from 'src/app/shared/event.model';
 @Component({
   selector: 'app-edit-schedule-page',
   templateUrl: './edit-schedule-page.component.html',
@@ -9,6 +11,14 @@ import {MatTableModule} from '@angular/material/table';
 })
 export class EditSchedulePageComponent {
 
+  Events: Observable<Event[]>;
   displayedColumns: string[] = ['name', 'type', 'end time', 'action'];
-
+  private endpoint = 'https://academic-calendar-backend.onrender.com/api/Events/'
+  dataSource: any;
+  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+ constructor(private http: HttpClient) { 
+   this.Events= this.http.get<Event[]>(this.endpoint);// ['SEG3102', 'SEG3101'];
+ 
+ }
+ 
 }
