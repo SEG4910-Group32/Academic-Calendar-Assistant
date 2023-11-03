2d4ea67574eac86c8952fafabcdcc3727a70fb9f
Ghazal Sadeghpour <gsade077@uottawa.ca>
2023-11-03 16:39:02 -0400

    the data shows correctly for My schedules

--- assistant-calendar/src/app/profile-page/profile-page.component.ts
+++ assistant-calendar/src/app/profile-page/profile-page.component.ts
@@ -16,26 +16,46 @@ import {ScrollingModule} from '@angular/cdk/scrolling';
 })
 export class ProfilePageComponent {
   //displayedColumns: string[] = ['Name', 'Id', 'Edit', 'Delete'];
-  Schedules: any;
-  // private endpoint = '"https://academic-calendar-backend.onrender.com/api/schedules/';
-  //private endpoint = 'https://academic-calendar-backend.onrender.com/api/owner/65175b6ec4cfd5b8effe44ee';
-  private endpoint = 'https://academic-calendar-backend.onrender.com/api/schedules/user/owns'
-   dataSource: any;
+  Schedules:  any;
+  subsSchedules: any;
+  dataOwns = [];
+  dataSubs = [];
+  private endpoint1 = 'https://academic-calendar-backend.onrender.com/api/schedules/user/owns'
+  private endpoint2 = 'https://academic-calendar-backend.onrender.com/api/schedules/user/subscribed'
+  
+  dataSource: any;
    items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
    
   constructor(private http: HttpClient) { 
     //this.Schedules= this.http.get<Deliverable[]>(this.endpoint);// ['SEG3102', 'SEG3101'];
     console.log(localStorage.getItem("currUser") )
-    this.Schedules = this.http.post(this.endpoint, { token: localStorage.getItem("currUser") });
-    
+    this.Schedules = this.http.post(this.endpoint1, { token: localStorage.getItem("currUser") });
+    this.subsSchedules = this.http.post(this.endpoint2, { token: localStorage.getItem("currUser") });
+    console.log("this.Schedules", this.Schedules);
+    console.log("this.subsSchedules", this.subsSchedules);
    this.Schedules.subscribe(
      (response:any) => {
-       console.log('POST request successful:', response);
+      this.dataOwns = response
+      if (response && response.schedules) {
+        // Extract the 'schedules' array from the JSON response
+        this.dataOwns = response.schedules;
+      }
+       console.log('POST request successful: this.dataOwns', this.dataOwns);
      },
      (error:any) => {
        console.error('POST request failed:', error);
      }
    );
+
+   this.subsSchedules.subscribe(
+    (response:any) => {
+     this.dataSubs = response.schedule
+      console.log('POST request successful:', this.dataSubs);
+    },
+    (error:any) => {
+      console.error('POST request failed:', error);
+    }
+  );
   }
   
 
