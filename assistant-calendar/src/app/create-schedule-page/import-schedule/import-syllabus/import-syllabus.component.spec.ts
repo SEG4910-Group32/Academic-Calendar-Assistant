3a1db4e2cd883be341cbb2cad38b9d6bc5e50f4f
YoussefJallouli032 <youssefjallouli14@gmail.com>
2023-11-09 21:54:04 -0500

    test: Implement and Refine Tests for ImportSyllabusComponent
    - Ensure ImportSyllabusComponent creation with basic instantiation test.
    - Simulate file drop handling to verify correct file processing.
    - Test the transformation of file sizes for accurate UI display.
    - Confirm functionality of file removal feature for user interaction.
    
    These tests aim to cover key functionalities and user interactions within the ImportSyllabusComponent, ensuring reliability and expected behavior.

--- assistant-calendar/src/app/create-schedule-page/import-schedule/import-syllabus/import-syllabus.component.spec.ts
+++ assistant-calendar/src/app/create-schedule-page/import-schedule/import-syllabus/import-syllabus.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ImportSyllabusComponent } from './import-syllabus.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ImportSyllabusComponent', () => {
   let component: ImportSyllabusComponent;
@@ -8,7 +10,12 @@ describe('ImportSyllabusComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ImportSyllabusComponent ]
+      declarations: [ImportSyllabusComponent],
+      imports: [
+        MatDialogModule,
+        BrowserAnimationsModule // Import BrowserAnimationsModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA] // Keep this to ignore unrecognized elements and attributes
     })
     .compileComponents();
 
@@ -19,5 +26,42 @@ describe('ImportSyllabusComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should handle file drop correctly', () => {
+    const mockFile: File = new File(
+      ["dummy content"], 
+      "0-ECO1504 C - Prof. Boubacar Moumouni.pdf", 
+      { type: "application/pdf" }
+    );
+
+    // Using type assertion to bypass TypeScript type checking
+    const mockFileEntry: any = {
+      isFile: true,
+      isDirectory: false,
+      name: mockFile.name,
+      file: (callback: (file: File) => void) => {
+        callback(mockFile);
+      }
+    };
+
+    const mockNgxFileDropEntry = {
+      fileEntry: mockFileEntry
+    };
+
+    component.fileUploadHandler([mockNgxFileDropEntry as any]);
+    expect(component.files.length).toBeGreaterThan(0);
+  });
+
+  it('should correctly transform file sizes', () => {
+    const size = component.transform(1024); // 1 KB
+    expect(size).toBe('1.00 KB');
+  });
+
+  it('should remove a file on removeFile call', () => {
+    const mockFile = { relativePath: 'test.pdf' };
+    component.data.push(mockFile as any);
+    component.removeFile(mockFile as any);
+    expect(component.data.length).toBe(0);
   });
 });
