b3df0572139f76d82a6901a31d68a156d3100b21
Ghazal Sadeghpour <gsade077@uottawa.ca>
2023-11-12 01:39:07 -0500

    show a message( a snackbar component) to confirm/deny schedule deletion

--- assistant-calendar/src/app/profile-page/delete-schedule/delete-schedule/delete-schedule.component.ts
+++ assistant-calendar/src/app/profile-page/delete-schedule/delete-schedule/delete-schedule.component.ts
@@ -5,7 +5,7 @@ import { ProfilePageComponent } from '../../profile-page.component';
 import { HttpClient } from '@angular/common/http';
 import {MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-delete-schedule',
   templateUrl: './delete-schedule.component.html',
@@ -19,21 +19,33 @@ export class DeleteScheduleComponent {
     public dialogRef: MatDialogRef<ProfilePageComponent>,
     private http: HttpClient,
     @Inject(MAT_DIALOG_DATA) public data: {name:string, id:string},
+    private _snackBar: MatSnackBar
   ) {}
 
-  deleteSchedule(scName:any){
+  deleteSchedule(scName:any):void{
     console.log("scName: ",scName)
     console.log("data.name", this.data.name)
     console.log("this.data.id ",this.data.id)
     console.log("scName == this.data.name", scName == this.data.name)
     if(scName == this.data.name){
-      this.deleteSched()
+      this.deleteSched();
+
+      this.openSnackbar('Schedule deleted successfully');
+      //this._snackBar.open("Schedule delete succesfully");
     }
     else{
       console.log("The name you entered is incorrect")
+      this.openSnackbar("Couldn't delete the schedule, please try again");
     }
   }
 
+
+  openSnackbar(message: string): void {
+    this._snackBar.open(message, 'Close', {
+      duration: 3000, // Set the duration in milliseconds (e.g., 3000 for 3 seconds)
+      panelClass: 'custom-snackbar',
+    });
+  }
   //method used to delete an schedule
 deleteSched() {
   const deleteUrl = 'https://academic-calendar-backend.onrender.com/api/schedules';
@@ -58,5 +70,9 @@ deleteSched() {
 }
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message);
   }
 }
